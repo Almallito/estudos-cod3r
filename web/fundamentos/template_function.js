6f8461b253c1be8f5dab2cb7c27d329d43ba0bf9
Almallito <djalma.hjr@gmail.com>
2020-07-27 09:11:37 -0300

    function

--- web/fundamentos/template_function.js
+++ web/fundamentos/template_function.js
@@ -6,6 +6,11 @@ const template = `
 console.log(template)
 
 const up = texto => texto.toUpperCase() // function arrow
+
 console.log(`Ei... ${up('cuidado')} ${nome}`)
 
 console.log(!!'')
+
+const soma = (a, b) => a + b
+
+console.log(soma(1, 2))
