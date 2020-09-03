59b05971151faaaea74f84495f933d57199c572f
almallito <djalma.hjr@gmail.com>
2020-09-03 17:14:07 -0300

    iniciando JQuery

---
+++ web/gulp/basico/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const series = gulp.series
+const parallel = gulp.parallel
+
+const antes1 = cb => {
+    console.log('Antes 1')
+    return cb()
+}
+const antes2 = cb => {
+    console.log('Antes 2')
+    return cb()
+}
+function copiar(cb) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+const fim = cb => {
+    console.log('Fim')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim
+    )
