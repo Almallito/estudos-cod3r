fc9dff502803e57abf43faed51536dfe5fe3dbf9
almallito <djalma.hjr@gmail.com>
2020-09-16 08:24:48 -0300

    projeto monty hall

---
+++ web/vue/calculator/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+  import Calculator from './main/Calculator'
+
+export default {
+  components: {Calculator}
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap");
+@font-face {
+  font-family: "Roboto Mono";
+}
+* {
+  font-family: "Roboto Mono", monospace;
+}
+body{
+  margin: 0;
+}
+#app{
+  display:flex;
+  flex-direction:column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73))
+}
+</style>
